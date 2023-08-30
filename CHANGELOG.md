303feb6d9878974cfb154eeb5e02163df1571087
Naglis <naglis1134@gmail.com>
2023-08-30 23:22:56 +0300

    Dark-light mode drawing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - More references
 - Add more words (from npw.lt physics)
+- Add nice drawing to dark-light mode
 
 ## Fixed
 
--- client/src/DarkModeButton.jsx
+++ client/src/DarkModeButton.jsx
@@ -4,18 +4,27 @@ import { BsMoonFill } from "react-icons/bs";
 
 export default function DarkModeButton({ isDarkMode, setIsDarkMode }) {
   return (
-    <Button
-      variant="secondary"
-      style={{
-        borderRadius: "100%",
-      }}
-      onClick={() => setIsDarkMode(!isDarkMode)}
-    >
-      {isDarkMode ? (
-        <FiSun style={{ marginBottom: "4px" }} />
-      ) : (
-        <BsMoonFill style={{ marginBottom: "4px" }} />
-      )}
-    </Button>
+    <>
+      <Button
+        variant="secondary"
+        style={{
+          borderRadius: "100%",
+        }}
+        onClick={() => setIsDarkMode(!isDarkMode)}
+      >
+        {isDarkMode ? (
+          <FiSun style={{ marginBottom: "4px" }} />
+        ) : (
+          <BsMoonFill style={{ marginBottom: "4px" }} />
+        )}
+      </Button>
+      <img
+        className="dark-light-mode-text"
+        src={`assets/${isDarkMode ? "light" : "dark"}-mode-${
+          innerWidth <= 768 ? "mobile" : "web"
+        }.png`}
+        alt="text 'light mode'"
+      />
+    </>
   );
 }
