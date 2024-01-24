72b0b2868b3a5c52289666dce39eb0d40429a6eb
Naglis <naglis1134@gmail.com>
2024-01-24 13:39:13 +0200

    Add contribute page with firebase

--- src/LTContributePage/index.tsx
+++ src/LTContributePage/index.tsx
@@ -1,25 +1,91 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Form, FormControl, FormLabel, Table } from "react-bootstrap";
+import { initializeApp } from "firebase/app";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+  runTransaction,
+} from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmOEHthPkRpM7RO40qJUvDpqa9fpUwUDk",
+  authDomain: "sci-dict.firebaseapp.com",
+  databaseURL:
+    "https://sci-dict-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sci-dict",
+  storageBucket: "sci-dict.appspot.com",
+  messagingSenderId: "859706346326",
+  appId: "1:859706346326:web:261df4d5b6f981bf74e4f7",
+  measurementId: "G-FNSF0SL3S4",
+};
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const wordsRef = ref(db, "words/");
 
 export default function LTContributePage() {
   const [input, setInput] = useState<{
     from?: string;
     en?: string;
     literature: string;
-  }>();
+  }>({ from: "", en: "", literature: "" });
 
   const [words, setWords] = useState<
     {
+      id: string;
       from?: string;
       en?: string;
       literature: string;
       likes: number;
     }[]
-  >([{ from: "test", literature: "test", likes: 0 }]);
+  >([]);
+
+  useEffect(() => {
+    onValue(wordsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        setWords(Object.values(data));
+      }
+    });
+  }, []);
+
+  const sendSubmitData = () => {
+    if (!input.en && !input.from && !input.literature) {
+      alert("Pala... Tai gi nieko neįvedei... :(");
+      return;
+    }
+    if (!input?.literature || !input?.from) {
+      alert("Literatūra ir lietuviška frazė yra privalomi");
+      return;
+    }
+
+    const newWordRef = push(wordsRef);
+    set(newWordRef, {
+      id: newWordRef.key,
+      from: input?.from,
+      en: input?.en,
+      literature: input?.literature,
+      likes: 0,
+    });
+    alert("Pateikta, ačiū!");
+  };
+
+  const incrementLike = (word: {
+    id: string;
+    from?: string;
+    en?: string;
+    literature: string;
+    likes: number;
+  }) => {
+    const db = getDatabase();
+    const wordRef = ref(db, `words/${word.id}`);
+    set(wordRef, { ...word, likes: word.likes + 1 });
+  };
 
   return (
     <>
-      <h1>DAR NEVEIKIA</h1>
       <p>
         Čia galite pateikti naujus žodžius su/be vertimų (būtina paminėti
         literatūrą, gali būti ir ne iš dabartinio sąrašo) - žodžiai bus
@@ -40,7 +106,7 @@ export default function LTContributePage() {
       <h3 style={{ marginTop: "20px", marginBottom: "20px" }}>
         Naujo žodžio / žodžių junginio pasiūlymas
       </h3>
-      <Form onSubmit={() => alert("thx")}>
+      <Form>
         <FormControl
           type="text"
           placeholder="Literatūroje..."
@@ -53,19 +119,19 @@ export default function LTContributePage() {
           type="text"
           placeholder="Lietuviškai rašoma..."
           className="mr-sm-2"
-          value={input?.literature}
-          onChange={(e) => setInput({ ...input, literature: e.target.value })}
+          value={input?.from}
+          onChange={(e) => setInput({ ...input, from: e.target.value })}
           style={{ marginBottom: "10px", border: "2px solid #1383bf" }}
         />
         <FormControl
           type="text"
           placeholder="Angliškai sugooglint galima..."
           className="mr-sm-2"
-          value={input?.literature}
-          onChange={(e) => setInput({ ...input, literature: e.target.value })}
+          value={input?.en}
+          onChange={(e) => setInput({ ...input, en: e.target.value })}
           style={{ marginBottom: "10px", border: "2px solid #1383bf" }}
         />
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={sendSubmitData}>
           Pateikti (bus matoma viešai)
         </Button>
       </Form>
@@ -84,21 +150,20 @@ export default function LTContributePage() {
               <th>Like'ai</th>
               <th>Nusiųsti like'ą</th>
             </tr>
-            {words.map((word) => (
-              <tr>
+            {words?.map((word) => (
+              <tr key={word.id}>
                 <td>{word.from ? word.from : <em>nėra vertimo</em>}</td>
                 <td>{word.en ? word.en : <em>no translation</em>}</td>
                 <td>{word.literature}</td>
                 <th>{word.likes}</th>
                 <th>
-                  <Button>Like</Button>
+                  <Button onClick={() => incrementLike(word)}>Like</Button>
                 </th>
               </tr>
             ))}
           </tbody>
         </Table>
       )}
-      {/* Reikia keiksmazodziu validacijos - alerta numest "AND... YOURE BANNED*/}
     </>
   );
 }
